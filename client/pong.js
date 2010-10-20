b780649cc4cfa4ffed526d3799937b73b873794b
Glenn Y. Rolland <glenux@gmail.com>
2010-10-20 21:24:11 +0200

    client: Removed useless references to socket.io .

--- client/pong.js
+++ client/pong.js
@@ -1,7 +1,20 @@
 (function(){
+	if ("WebSocket" in window) {
+		var ws = new WebSocket("ws://example.com/service");
+		ws.onopen = function() {
+			// Web Socket is connected. You can send data by send() method.
+			ws.send("message to send"); ....
+		};
+		ws.onmessage = function (evt) { var received_msg = evt.data; ... };
+		ws.onclose = function() { // websocket is closed. };
+	} else {
+		// use Socket.io instead
+	}
+
 	var canvasDom = document.getElementById("jeu");
 	var c = canvasDom.getContext("2d");
 	
 	c.fillStyle="#000";
 	c.rect(0,0,10,10);
+
 })();
