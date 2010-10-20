7f80d1b2fe1663704f34651737580daa6d1e62ec
Philippe <philippe@ideapad.(none)>
2010-10-20 21:09:02 +0200

    touchf

--- client/pong.js
+++ client/pong.js
@@ -4,5 +4,19 @@
 	
 	c.fillStyle="#000";
 	c.fillRect(0,0,100,100);
-	
+
+document.addEventListener( 'touchstart', function ( event ) {
+        event.preventDefault();
+        for ( var i = 0; i < event.touches.length; i++ ) {
+            console.log('start'+event.touches[i].pageX,
+            event.touches[i].pageY);
+        }
+    }, false );
+    document.addEventListener( 'touchmove', function ( event ) {
+        event.preventDefault();
+        for ( var i = 0; i < event.touches.length; i++ ) {
+            console.log('move'+event.touches[i].pageX,
+            event.touches[i].pageY);
+        }
+    }, false );	
 })();
